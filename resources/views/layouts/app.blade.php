4feda553d2b412a948620217d710dd954709ea96
erzan <do.earljan@gmail.com>
2026-05-09 06:36:21 +0800

    fix: broken image logo in admin side

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -60,7 +60,8 @@
         <!-- Brand Logo -->
         <a href="/" class="brand-link">
             @laravelPWA
-            <img src="https://trace.southernleyte.org.ph/assets/img/slsu-logo.png" alt="AdminLTE Logo"
+            {{-- <img src="https://trace.southernleyte.org.ph/assets/img/slsu-logo.png" alt="AdminLTE Logo" --}}
+            <img src="/images/AdminLTELogo.png" alt="AdminLTE Logo"
                  class="brand-image img-circle elevation-3"
                  style="opacity: .8">
             <span class="brand-text font-weight-light">SLSU - EVENT</span>
