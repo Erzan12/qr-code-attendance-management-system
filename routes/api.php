ff292086116628bba1a0f15115639c49e0df91a3
erzan12 <do.earljan@gmail.com>
2026-09-03 14:45:31 +0800

    feat: fix typo error in api for notifications

--- routes/api.php
+++ routes/api.php
@@ -38,7 +38,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/events/{event}', [EventController::class, 'show']);
 
     // Notifications
-    Route::get('/notificaitons', [NotificationController::class, 'index']);
+    Route::get('/notifications', [NotificationController::class, 'index']);
     Route::get('/notifications/unread-count', [NotificationController::class, 'unreadCount']);
     Route::get('/notifications/{notification}/read', [NotificationController::class, 'markRead']);
     Route::get('/notifications/read-all', [NotificationController::class, 'markAllRead']);
